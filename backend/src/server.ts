4559241054f94c979faa67d69f6712917d148216
Muchiripatrick1 <90451820+Muchiripatrick1@users.noreply.github.com>
2025-04-13 01:04:10 +0200

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,12 +2,12 @@ import mongoose from "mongoose";
 import app from "./app";
 import env from "./env";
 
-const port = Number(process.env.PORT) || 10000;
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(env.MONGO_CONNECTION_STRING)
 .then(()=>{
     console.log("mongoose connected")
-    app.listen(port, '0.0.0.0', ()=> console.log("server running on port"+ port));
+    app.listen(PORT, ()=> console.log("server running on port"+ PORT));
 })
 .catch(
     console.error
