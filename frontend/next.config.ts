6ea4e9dd302a5b1bc4b42706c7864c1468d5e8ad
Muchiripatrick1 <90451820+Muchiripatrick1@users.noreply.github.com>
2025-04-13 20:06:38 +0200

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   images: {
-    domains: ["localhost"],
+    domains: ["https://flock-talk.onrender.com"],
     deviceSizes: [576, 768, 992, 1200, 1400]
   },
   eslint: {
